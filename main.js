44267a9d3a4531a7ddb26cc1968b9ef2bf60f1fe
오형욱 <ohhyungook@ohyeongugui-MacBook-Pro.local>
2020-10-19 21:51:56 +0900

    게임 시작 버튼, 스코어, 타이머 조작

--- main.js
+++ main.js
@@ -1,14 +1,60 @@
 "use strict";
 const CARROT_size = 80;
-const filed = document.querySelector(".mainBody");
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
+const filed = document.querySelector(".game__field");
 const filedRect = filed.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button')
+const gameTimer = document.querySelector('.game__timer')
+const gameScore = document.querySelector('.game__score')
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+  // console.log('log')
+  if(started) {
+    stopGame();
+  }else {
+    startGame();
+  }
+  started = !started;
+})
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerScore();
+}
+
+function showTimerScore() {
+  gameTimer.style.visibility = 'visible';
+  gameScore.style.visibility = 'visible';
+}
+
+function stopGame() {
+
+}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector('.fa-play');
+  icon.classList.add('fa-stop'); // 점 빼자
+  icon.classList.remove('fa-play');
+  console.log(icon)
+}
+
 
 function initGame() {
   //벌레와 당근을 생성한뒤 filed에 추가해줌
 
-  console.log(filedRect);
-  additem("carrot", 5, "img/carrot.png");
-  additem("bug", 5, "img/bug.png");
+  // console.log(filedRect);
+  additem("carrot", CARROT_COUNT, "img/carrot.png");
+  additem("bug", BUG_COUNT, "img/bug.png");
+  
+  // 당근 개수 count에 표시 
+  // carrotCount();
 }
 
 function additem(className, count, imgPath) {
@@ -29,9 +75,20 @@ function additem(className, count, imgPath) {
     filed.appendChild(item);
   }
 }
-
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-initGame();
+
+// function carrotCount() {
+//   const carrot = document.querySelectorAll('.carrot')
+//   const count = document.querySelector('.count')
+//   console.log(carrot.length)
+//   console.log(count.innerHTML)
+
+//   count.innerHTML = `${carrot.length}`;
+
+// }
+
+
+
